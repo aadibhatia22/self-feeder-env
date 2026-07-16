dbd96e5eeae18694a032787e5fcd3cf04abbd2ed
aadibhatia22 <aadi.bhatia22@gmail.com>
2026-07-16 18:39:12 -0400

    number of objects randomized working

--- CODE/Enviornment/enviornment_randomizer.py
+++ CODE/Enviornment/enviornment_randomizer.py
@@ -5,7 +5,7 @@ class Enviornment_Randomizer:
 
     
     def __init(self):
-        self.rng = np.random.default_rng()
+        pass 
 
     def randomize_color (self, model, geom_name):
         model.geom(geom_name).rgba[:3] = np.random.rand(3)  # Random RGB values
@@ -14,21 +14,29 @@ class Enviornment_Randomizer:
     def randomize_number_of_objects(self, list_of_food_object_names, model, min_objects, in_secene_z_cord, out_of_scene_z_cord):
         number_of_objects_initilized = 0
         while True:
-            random_number = self.rng.random()
-            if random_number<0.5 and number_of_objects_initilized>min_objects:
+            rng = np.random.default_rng()
+            random_number = rng.random()
+            if random_number<0.5 and number_of_objects_initilized>=min_objects:
                 break
             else:
-                if(list_of_food_object_names.ndim > 1):
+                if(list_of_food_object_names.ndim != 1):
                     break
                 
-                size = list_of_food_object_names.size()
+                size = list_of_food_object_names.size
                 #already in index position
-                object_index = self.rng.integers(0, size)
+                object_index = -1
+                if(size <= 1):
+                    object_index = 0
+                else:
+                    object_index = rng.integers(0, size)
                 name_of_object = list_of_food_object_names[object_index]
-                model.geom[name_of_object].pos[2] = in_secene_z_cord
-                np.delete(list_of_food_object_names, name_of_object)
+                model.body(name_of_object).pos[2] = in_secene_z_cord
+                list_of_food_object_names = np.delete(list_of_food_object_names, object_index)
+                number_of_objects_initilized += 1
+                
 
-            for object_name in list_of_food_object_names:
-                model.geom[object_name].pos[2] = out_of_scene_z_cord
-                np.delete(list_of_food_object_names, object_name)
-        return model
+        for object_name in list_of_food_object_names:
+            object_index = np.argwhere(list_of_food_object_names == object_name)
+            model.body(object_name).pos[2] = out_of_scene_z_cord
+            list_of_food_object_names = np.delete(list_of_food_object_names, object_index)
+        return model 
